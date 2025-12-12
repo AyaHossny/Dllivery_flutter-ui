a4bf2410f268600067cd93da7900aa33a0c134a2
AyaHossny <ayahossny590@gmail.com>
2025-12-12 03:07:11 +0200

    Add files via upload

---
+++ fadeAnimation.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+class FadeAnimation extends StatelessWidget {
+final double delay;
+final Widget child;
+
+const FadeAnimation({
+super.key,
+required this.delay,
+required this.child,
+});
+
+@override
+Widget build(BuildContext context) {
+return PlayAnimationBuilder<double>(
+delay: Duration(milliseconds: (500 * delay).round()),
+duration: const Duration(milliseconds: 600),
+tween: Tween(begin: 0.0, end: 1.0),
+curve: Curves.easeOut,
+builder: (context, value, _) {
+return Opacity(
+opacity: value,
+child: Transform.translate(
+offset: Offset(120 * (1 - value), 0),
+child: child,
+),
+);
+},
+);
+}
+}
+
